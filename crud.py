55647adc95aa21c0153514ee2f2ee9c31137e9ce
Kaylan Teodoro <kaylanteodoro344@gmail.com>
2025-10-06 16:37:36 -0300

    add requirements.txt

--- crud.py
+++ crud.py
@@ -4,13 +4,14 @@ def criar_aluno(nome, idade):
     conexao, cursor = conectar()
     if conexao:
         try:
-            cursor.execute(
-                "INSERT INTO alunos (nome, idade) VALUES (%s, %s)", 
-                (nome, idade)
-                )
-            conexao.commit()
+            if idade >= 16:
+                cursor.execute(
+                    "INSERT INTO alunos (nome, idade) VALUES (%s, %s)", 
+                    (nome, idade)
+                    )
+                conexao.commit()
         except Exception as erro:
-            print(f"Erro ao criar aluno: {erro}")
+                print(f"Erro ao criar aluno: {erro}")
         finally:
             cursor.close()
             conexao.close()
