796e33d0fb5966baa94673829c18b274dac0d57b
Kaylan Teodoro <kaylanteodoro344@gmail.com>
2025-10-06 14:13:59 -0300

    função listar alunos ✔

--- db.py
+++ db.py
@@ -18,7 +18,10 @@ def conectar():
     try:
         conexao = pg.connect(**params)
         cursor = conexao.cursor()
+        print("Deu certo!!!!")
         return conexao,cursor
     except Exception as erro:
         print(f"Erro de conexão {erro}")
         return None, None
+
+conectar()
