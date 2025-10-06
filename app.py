f28bb62c8803a6462fbbf214505e4b1966d62517
Kaylan Teodoro <kaylanteodoro344@gmail.com>
2025-10-06 16:13:11 -0300

    add função atualizar idade no app.py ✔

--- app.py
+++ app.py
@@ -27,3 +27,15 @@ elif menu == "Listar":
     else:
         st.info("Nenhum aluno encontrado")
 
+elif menu == "Atualizar":
+    st.subheader("Atualizar idade")
+    alunos = listar_alunos()
+    if alunos:
+        id_aluno = st.selectbox("Escolha o id do aluno para atualizar", [linha[0] for
+         linha in alunos])
+        nova_idade = st.number_input("Nova idade", min_value=16, step=1)
+        if st.button("Atualizar"):
+            atualizar_idade(id_aluno, nova_idade)
+            st.success(f"Idade do aluno atualizada com sucesso.")
+    else:
+        st.info("Nenhum aluno disponível para atualizar")
